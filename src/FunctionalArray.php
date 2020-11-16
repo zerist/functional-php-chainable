6e7b6462333240f078d9b76a1e7ff95d53b5f2f9
zerist <15871792739@163.com>
2020-11-16 20:09:25 +0800

    #todo: lazy call based on array_reduce and decorator

--- src/FunctionalArray.php
+++ src/FunctionalArray.php
@@ -4,6 +4,7 @@ namespace Kanye\Functional;
 use function Functional\false;
 use function Functional\true;
 
+//todo 重构，装饰器模式，array_reduce倒序调用callbacks
 /**
  * array wrapped with chainable function
  * Class FunctionalArray
@@ -14,6 +15,7 @@ class FunctionalArray
     private array $originArray = [];
     private array $resultArray = [];
     private bool $lastActionStatus = true;
+    private array $callbacks = []; //todo
 
     public function __construct(array $array){
         $this->originArray = $array;
@@ -47,6 +49,15 @@ class FunctionalArray
             }
         }
         $this->updateNestedStatus();
+        return $this;
+    }
+
+    public function any(callable $callback) {
+        foreach ($this->resultArray as $key => $value) {
+            if ($callback($value, $key)) {
+                $this->lastActionStatus = true;
+            }
+        }
         return $this;
     }
 
