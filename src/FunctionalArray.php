5f97e6f85438b2a990338e82546604507f42712a
zerist <15871792739@163.com>
2020-11-17 23:22:31 +0800

    #feature:lazy functional array

--- src/FunctionalArray.php
+++ src/FunctionalArray.php
@@ -1,82 +1,56 @@
 <?php
 namespace Kanye\Functional;
 
-use function Functional\false;
-use function Functional\true;
-
-//todo 重构，装饰器模式，array_reduce倒序调用callbacks
 /**
  * array wrapped with chainable function
  * Class FunctionalArray
  * @package Kanye\Functional
  */
-class FunctionalArray
-{
-    private array $originArray = [];
-    private array $resultArray = [];
-    private bool $lastActionStatus = true;
-    private array $callbacks = []; //todo
-
-    public function __construct(array $array){
-        $this->originArray = $array;
-        $this->resultArray = $array;
-    }
-
+class FunctionalArray extends FunctionalBase {
     public function each(callable $callback) {
         $r = [];
-        foreach ($this->resultArray as $value) {
-            $r[] = $callback($value);
+        foreach ($this->resultData as $key => $value) {
+            $r[$key] = $callback($value, $key);
         }
-        $this->resultArray = $r;
+        $this->resultData = $r;
         return $this;
     }
 
     public function filter(callable $callback) {
         $r = [];
-        foreach ($this->resultArray as $key => $value) {
+        foreach ($this->resultData as $key => $value) {
             if ($callback($value, $key)) {
                 $r[$key] = $value;
             }
         }
-        $this->resultArray = $r;
+        $this->resultData = $r;
         return $this;
     }
 
     public function all(callable $callback) {
-        foreach ($this->resultArray as $key => $value) {
+        foreach ($this->resultData as $key => $value) {
             if (!$callback($value, $key)) {
-                $this->lastActionStatus = false;
+                $this->updateNestedStatus(false);
+                break;
             }
         }
-        $this->updateNestedStatus();
         return $this;
     }
 
     public function any(callable $callback) {
-        foreach ($this->resultArray as $key => $value) {
+        foreach ($this->resultData as $key => $value) {
             if ($callback($value, $key)) {
-                $this->lastActionStatus = true;
+                $this->updateNestedStatus(true);
             }
         }
         return $this;
     }
 
-    public function getOriginData() {
-        return $this->originArray;
-    }
-
-    public function getResult() {
-        return $this->resultArray;
-    }
-
-    public function test() {
-        echo "hello packagist";
-    }
-
-    private function updateNestedStatus()
+    protected function updateNestedStatus(bool $status)
     {
+        $this->lastActionStatus = $status;
         if ($this->lastActionStatus === false) {
-            $this->resultArray = [];
+            $this->resultData = [];
         }
     }
 }
---
+++ src/LazyFunctionalArray.php
@@ -0,0 +1,63 @@
+<?php
+namespace Kanye\Functional;
+
+//todo 大循环协程优化
+//todo memorize
+use function MongoDB\BSON\toJSON;
+
+class LazyFunctionalArray extends FunctionalBase {
+    protected string $initialFunctionName = 'initial';
+
+    protected function updateNestedStatus(bool $status)
+    {
+        $this->lastActionStatus = $status;
+        if ($this->lastActionStatus === false) {
+            $this->resultData = [];
+        }
+    }
+
+    public function each(callable $callback) {
+        $this->callbacks[] = function () use ($callback) {
+            $r = [];
+            foreach ($this->resultData as $key => $value) {
+                $r[$key] = $callback($value, $key);
+            }
+            $this->resultData = $r;
+        };
+        return $this;
+    }
+
+    public function filter($callback) {
+        $this->callbacks[] = function () use ($callback) {
+            $r = [];
+            foreach ($this->resultData as $key => $value) {
+                if ($callback($value, $key)) {
+                    $r[$key] = $value;
+                }
+            }
+            $this->resultData = $r;
+        };
+        return $this;
+    }
+
+    public function get() {
+        $this->runCallbacks();
+        return $this->resultData;
+    }
+
+    public function __toString()
+    {
+        return json_encode($this->get());
+    }
+
+    protected function initial() {
+        //todo 初始化启动，清理
+    }
+
+    protected function runCallbacks() {
+        array_reduce($this->callbacks, function ($initial, $callback) {
+            $callback();
+            return $this;
+        }, $this->initialFunctionName);
+    }
+}
