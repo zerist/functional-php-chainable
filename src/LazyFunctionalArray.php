9b15512d75b9b04b0bad242e8b2b7b276f7cb24b
zerist <15871792739@163.com>
2020-12-01 19:34:47 +0800

    #feature:lazy functional array

--- src/LazyFunctionalArray.php
+++ src/LazyFunctionalArray.php
@@ -3,7 +3,6 @@ namespace Kanye\Functional;
 
 //todo 大循环协程优化
 //todo memorize
-use function MongoDB\BSON\toJSON;
 
 class LazyFunctionalArray extends FunctionalBase {
     protected string $initialFunctionName = 'initial';
